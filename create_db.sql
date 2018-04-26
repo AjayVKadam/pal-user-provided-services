cc2cd8681d6e42476883da631cd74b747926e420
nagavejju <nagesh_vejju@yahoo.com>
2018-04-26 08:39:43 -0600

    create table

---
+++ create_db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE cohort (
+  id  INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(64),
+  nickname VARCHAR(64),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO cohort (name) VALUES ('Ajay');
+INSERT INTO cohort (name) VALUES ('Mahanthi');
